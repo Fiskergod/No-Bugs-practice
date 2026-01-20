b02c8596edbdfed9eed79a76fe4048fc81e309e2
Evgenii <78149143+Fiskergod@users.noreply.github.com>
2026-01-20 12:25:35 +0100

    Create Counter.java

---
+++ Homework9/Task4/Counter.java
@@ -0,0 +1,13 @@
+package homework_9.task_4;
+
+public class Counter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized int getCount() {
+        return this.count;
+    }
+}
