da78b0e81baa7c5135e70b9a776eb388064b9465
Evgenii <78149143+Fiskergod@users.noreply.github.com>
2026-01-20 12:25:19 +0100

    Create Main.java

---
+++ Homework9/Task4/Main.java
@@ -0,0 +1,27 @@
+package homework_9.task_4;
+
+public class Main {
+    static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Потоки завершили работу. Счетчик = " + counter.getCount());
+    }
+}
