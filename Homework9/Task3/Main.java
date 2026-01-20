daf0f4b12996c004068d327d6e0f1affc1c06977
Evgenii <78149143+Fiskergod@users.noreply.github.com>
2026-01-20 12:24:25 +0100

    Create Main.java

---
+++ Homework9/Task3/Main.java
@@ -0,0 +1,8 @@
+package homework_9.task_3;
+
+public class Main {
+    static void main(String[] args) {
+        VolatileUse volatileUse = new VolatileUse();
+        volatileUse.threadCounter();
+    }
+}
