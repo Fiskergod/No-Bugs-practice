7afd5e88af2003328bebfc0172bbcc3e3ea44851
Evgenii <78149143+Fiskergod@users.noreply.github.com>
2026-03-05 21:18:54 +0100

    Create NumericalProcessorTest.java

---
+++ Homework10/numerical_methods/NumericalProcessorTest.java
@@ -0,0 +1,13 @@
+package homework_10.numerical_methods;
+
+import homework_10.NumericalProcessor;
+import org.junit.jupiter.api.BeforeEach;
+
+public class NumericalProcessorTest {
+    protected NumericalProcessor numericalProcessor;
+
+    @BeforeEach
+    public void setupTest() {
+        numericalProcessor = new NumericalProcessor();
+    }
+}
