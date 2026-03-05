afdfb9fcc186d5d8a844801dc0f0906f39d13712
Evgenii <78149143+Fiskergod@users.noreply.github.com>
2026-03-05 21:24:54 +0100

    Create StringProcessorTest.java

---
+++ Homework10/string_methods/StringProcessorTest.java
@@ -0,0 +1,13 @@
+package homework_10.string_methods;
+
+import homework_10.StringProcessor;
+import org.junit.jupiter.api.BeforeEach;
+
+public class StringProcessorTest {
+    protected StringProcessor stringProcessor;
+
+    @BeforeEach
+    public void setupTest() {
+        stringProcessor = new StringProcessor();
+    }
+}
