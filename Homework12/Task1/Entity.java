0a4262ef831ade601b8c225a64b8c7f0fa716a3b
Evgenii <78149143+Fiskergod@users.noreply.github.com>
2026-05-07 22:36:30 +0200

    Create Entity.java

---
+++ Homework12/Task1/Entity.java
@@ -0,0 +1,17 @@
+package homework_12.task_1;
+
+public abstract class Entity {
+    private String name;
+    private int age;
+    private boolean isActive;
+
+    public Entity(String name, int age, boolean isActive) {
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public boolean isActive() { return isActive; }
+}
