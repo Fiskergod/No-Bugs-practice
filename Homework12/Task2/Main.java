178d6fb24cd365470e4a581067f60cc237d90cc1
Evgenii <78149143+Fiskergod@users.noreply.github.com>
2026-05-07 22:38:16 +0200

    Create Main.java

---
+++ Homework12/Task2/Main.java
@@ -0,0 +1,27 @@
+package homework_12.task_2;
+
+public class Main {
+    static void main(String[] args) throws InvalidUserException {
+        // Тестовые пользователи
+        User validUser = new User("Иван", 25, "ivan@example.com");
+        User invalidNameUser = new User("петр", 30, "petr@example.com");
+        User invalidAgeUser = new User("Мария", 16, "maria@example.com");
+        User invalidEmailUser = new User("Анна", 35, "anna@example");
+
+        // Валидация включена
+        UserValidator.setValidationEnabled(true);
+
+        UserValidator.validate(validUser);
+        UserValidator.validate(invalidNameUser);
+        UserValidator.validate(invalidAgeUser);
+        UserValidator.validate(invalidEmailUser);
+
+        // Валидация выключена
+        UserValidator.setValidationEnabled(false);
+
+        UserValidator.validate(validUser);
+        UserValidator.validate(invalidNameUser);
+        UserValidator.validate(invalidAgeUser);
+        UserValidator.validate(invalidEmailUser);
+    }
+}
