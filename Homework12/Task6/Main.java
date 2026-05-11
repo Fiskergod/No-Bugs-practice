d2fab5d179649447fd026dd93d881dac95f80e86
Evgenii <78149143+Fiskergod@users.noreply.github.com>
2026-05-11 15:55:52 +0200

    Update Main.java

--- Homework12/Task6/Main.java
+++ Homework12/Task6/Main.java
@@ -7,12 +7,12 @@ public class Main {
     static void main(String[] args) {
         TaskService<Number> taskService = new TaskService<>();
 
-        Task<Number> task = new Task<>(1, "High",
-                LocalDate.now(), false);
-        Task<Number> task2 = new Task<>(2, "Low",
-                LocalDate.of(2023, Month.AUGUST, 11), true);
-        Task<Number> task3 = new Task<>(3, "Medium",
-                LocalDate.of(2025, Month.JANUARY, 1), true);
+        Task<Number> task = new Task<>(1, Priority.HIGH,
+                LocalDate.now(), Status.COMPLETED);
+        Task<Number> task2 = new Task<>(2, Priority.LOW,
+                LocalDate.of(2023, Month.AUGUST, 11), Status.IN_PROGRESS);
+        Task<Number> task3 = new Task<>(3, Priority.MEDIUM,
+                LocalDate.of(2025, Month.JANUARY, 1), Status.COMPLETED);
 
         taskService.addTask(task);
         taskService.addTask(task2);
@@ -20,8 +20,8 @@ public class Main {
 
         taskService.removeTaskById(2);
 
-        taskService.getTaskByPriority("High");
-        taskService.getTaskByStatus(false);
+        taskService.getTaskByPriority(Priority.HIGH);
+        taskService.getTaskByStatus(Status.COMPLETED);
         taskService.getSortedTaskByDate();
 
         System.out.println("\nВсе задачи: " + taskService.getAll());
